7804e41170979261dd6e8cd9b048a1b16e5f5450
John L Chen <zuan@google.com>
2020-08-23 00:19:09 +0800

    Add API definition for scoreboard.

---
+++ kofserver/scoreboard.py
@@ -0,0 +1,102 @@
+# Copyright (c) 2020 HITCON Agent Contributors
+# See CONTRIBUTORS file for the list of HITCON Agent Contributors
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# This hosts the ScoreBoard class which records the player's score and other stats.
+
+class ScoreBoard:
+    def __init__(self):
+        # TODO
+        pass
+
+    # This is called every time the Game logic checks if the players are alive.
+    # This method is used to record players who are alive.
+    # gameName: A string that is the name of the game that we are recording.
+    # playerName: An array of player's name.
+    # portUptime: An array of uptime for the player's port.
+    # portScorePerSec: An array of scores per second of uptime for the player.
+    # pidUptime: An arrya of uptime for the player's process.
+    # pidScorePerSec: An array of scores per second of uptime for the player.
+    # Array sizes for playerName, portUptime, portScorePerSec, pidUptime and
+    # pidScorePerSec are guaranteed to be the same.
+    # portUptime and pidUptime is usually the check interval.
+    # 
+    # For example:
+    # - In the past 30 seconds:
+    #     - Player Alice's process is online, but the port is unreachable.
+    #     - Player Bob's process is online, and the port is reachable.
+    #     - Player Eve's process and port are both offline.
+    # - In this round:
+    #     - Process being up is worth 5 points per seconds.
+    #     - Port reachable is worth 10 points per seconds.
+    # Then, the call to LogTicks will look like this:
+    # LogTicks(
+    #     "Game1",                   # gameName
+    #     [ "Alice", "Bob", "Eve" ], # playerName
+    #     [ 0, 30, 0 ],              # portUptime
+    #     [ 10, 10, 10 ],            # portScorePerSec
+    #     [ 30, 30, 0 ],             # pidUptime
+    #     [ 5, 5, 5 ]                # pidScorePerSec
+    # )
+    # In this case, Alice should get 30*5 points, Bob should get 30*5+30*10
+    # points, and Eve doesn't get any point.
+    def LogTicks(gameName, playerName, portUptime, portScorePerSec, pidUptime, pidScorePerSec):
+        # TODO
+        pass
+
+    # Logs the player's action, such as "Shellcode", "Command", and "OpenURL".
+    # gameName: a string that is the name of the game that we are recording.
+    # playerName: Name of the player doing the action.
+    # actionType: A string that represents the action type.
+    #             Currently, "Shellcode", "Command" and "OpenURL" is possible.
+    #             Others could be added in the future.
+    # actionContent: A string that represents the content of the action. For
+    #                shellcode, this would be the base64 of the shellcode.
+    #                For command, this is the full command, and for open URL,
+    #                This is the full URL.
+    #                Note that this field could be rather large, and it doesn't
+    #                need to be searched. (ie. no index, could use blob type or
+    #                even off database storage)
+    # Example:
+    # LogPlayerAction("game1", "malice", "command", "rm -rf /")
+    def LogPlayerAction(gameName, playerName, actionType, actionContent):
+        # TODO
+        pass
+
+    # Query the current score, returns an array of dict of score and other
+    # statistics. The returned dict should contain:
+    # - "playerName"
+    # - "portUptime"
+    # - "portScore"
+    # - "pidUptime"
+    # - "pidScore"
+    # - "totalScore"
+    # If gameName is "", then scores for all games are queried and the results
+    # are summed for each player.
+    # If playerName is "", then all players with non-zero scores are returned.
+    # If there's a problem with the database, an exception is raised.
+    # Example:
+    # If the example above on LogTicks is called twice, then:
+    # QueryScore("game1", "Alice") should results in:
+    # [ { "playerName": "Alice", "portUptime": 0, "portScore": 0,
+    #     "pidUptime": 60, "pidScore": 300, "totalScore": 300 } ]
+    def QueryScore(gameName, playerName):
+        # TODO
+        return []
