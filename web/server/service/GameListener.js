1fa7bc216e12b2ae6f067d0c5acbe88afe0f0b89
jim <gcobcqwe@yahoo.com.tw>
2020-09-08 23:29:19 +0800

    Rename game listener and game.yml

--- web/server/service/GameListener.js
+++ web/server/service/GameListener.js
@@ -21,20 +21,24 @@
  * SOFTWARE.
  */
 
-module.exports = class Game {
+module.exports = class GameListener {
     constructor(configManager, broadcast) {
         this.configManager = configManager;
         this.broadcast = broadcast;
     }
 
     run(gameName, agent) {
-        // Emit event notification to frontend
-        const eventCallback = (data) => this.broadcast.all('event', JSON.stringify(data));
-        agent.event(gameName, eventCallback);
+        try{
+            // Emit event notification to frontend
+            const eventCallback = (data) => this.broadcast.all('event', JSON.stringify(data));
+            agent.event(gameName, eventCallback);
 
-        // Emit score list to frontend
-        const scoreListCallback = (data) => this.broadcast.all('scoreList', JSON.stringify(data));
-        const interval = this.configManager.getConfig(['queryScoreIntervalMillisecond']);
-        setInterval(function () { agent.queryScore(gameName, scoreListCallback) }, interval);
+            // Emit score list to frontend
+            const scoreListCallback = (data) => this.broadcast.all('scoreList', JSON.stringify(data));
+            const interval = this.configManager.getConfig(['queryScoreIntervalMillisecond']);
+            setInterval(function () { agent.queryScore(gameName, scoreListCallback) }, interval);
+        } catch(err) {
+            console.error(err);
+        }
     }
 }
