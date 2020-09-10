0b4c2be6389ae3c7e5015259c3a666af719e7236
John L Chen <zuan@google.com>
2020-09-11 03:56:25 +0800

    Fix intro startup.

--- kofserver/scenario/Intro/Extra/startup.sh
+++ kofserver/scenario/Intro/Extra/startup.sh
@@ -7,14 +7,13 @@ chmod 000 /bin/kill
 chmod 000 /usr/bin/killall
 
 if [[ -b /dev/sdc ]]; then
-    if ! [[ -d "/var/www/html/storage.orig" ]];
-    then
+    if ! [[ -d "/var/www/html/storage.orig" ]]; then
         mv /var/www/html/storage /var/www/html/storage.orig
         mkdir /var/www/html/storage
-        mount /dev/sdc /var/www/html/storage
+    fi
+    mount /dev/sdc /var/www/html/storage
+    if ! [[ -f "/var/www/html/storage/index.html" ]]; then
         cp -pR /var/www/html/storage.orig/* /var/www/html/storage/.
-    else
-        mount /dev/sdc /var/www/html/storage
     fi
 fi
 
