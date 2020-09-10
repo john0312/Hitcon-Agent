61563809f0e8d947c1f60059ab3f48331dc63861
John L Chen <zuan@google.com>
2020-09-11 05:32:51 +0800

    DoorCloses scenario

---
+++ kofserver/scenario/DoorCloses/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Prepare the disk image and and dom XML.
+disk_path="${TMPIMAGEDIR}/${VMID}.qcow2"
+agent_path="${IMAGEDIR}/${VMNAME}.agent.sqfs"
+cp -L "${IMAGEDIR}/${VMNAME}.qcow2" "${disk_path}"
+xml_path="${TMPIMAGEDIR}/${VMID}.xml"
+AltVMName=`echo "MPG${VMID}" | sed "s#-##g"`
+# WARNING: Make sure VMID and disk_path is sanitized from the python side.
+cat "${VMDIR}/dom.xml" | sed "s#__VMID__#${VMID}#g" | sed "s#__DISKIMG__#${disk_path}#g" | sed "s#__VMNAME__#${AltVMName}#g" | sed "s#__AGENT_IMG__#${agent_path}#g" | sed "s#__STORIMG__#${stor_path}#g" > ${xml_path}
+
+sudo virsh define "${xml_path}"
